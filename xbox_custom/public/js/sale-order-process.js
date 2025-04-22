cb12256561b146830f4d383342edd4e5bcf36c0d
Do Ngoc Tu <tdngoctu@gmail.com>
2025-04-22 23:23:18 +0700

    feat: update when remove a row tax and charge

--- xbox_custom/public/js/sale-order-process.js
+++ xbox_custom/public/js/sale-order-process.js
@@ -6,79 +6,87 @@ const workflowActions = {
 };
 
 async function update_advance_and_outstanding(frm) {
-	try {
-		// Bắt hệ thống tính lại grand_total trước
-		await frm.script_manager.trigger("calculate_taxes_and_totals");
+  try {
+    // Bắt hệ thống tính lại grand_total trước
+    await frm.script_manager.trigger("calculate_taxes_and_totals");
 
-		let r = await vhtfm.call({
-			method: "vhtfm.client.get_list",
-			args: {
-				docmeta: "Payment Entry",
-				filters: {
-					payment_type: "Receive",
-					sales_order: frm.doc.name,
-					docstatus: 1
-				},
-				fields: ["name", "paid_amount"]
-			}
-		});
+    let r = await vhtfm.call({
+      method: "vhtfm.client.get_list",
+      args: {
+        docmeta: "Payment Entry",
+        filters: {
+          payment_type: "Receive",
+          sales_order: frm.doc.name,
+          docstatus: 1,
+        },
+        fields: ["name", "paid_amount"],
+      },
+    });
 
-		if (r.message) {
-			let total_advance = 0;
-			r.message.forEach(pe => {
-				total_advance += flt(pe.paid_amount);
-			});
+    if (r.message) {
+      let total_advance = 0;
+      r.message.forEach((pe) => {
+        total_advance += flt(pe.paid_amount);
+      });
 
-			// Sau khi chắc chắn grand_total đã được tính lại
-			let grand_total = flt(frm.doc.grand_total);
-			let advance_paid = flt(frm.doc.advance_paid || total_advance);
-			let outstanding_amount = grand_total - advance_paid;
+      // Sau khi chắc chắn grand_total đã được tính lại
+      let grand_total = flt(frm.doc.grand_total);
+      let advance_paid = flt(frm.doc.advance_paid || total_advance);
+      let outstanding_amount = grand_total - advance_paid;
 
-			await frm.set_value("advance_paid_temp", total_advance);
-			await frm.set_value("outstanding_amount", outstanding_amount);
-		}
-	} catch (err) {
-		console.error("Lỗi khi cập nhật:", err);
-	}
+      await frm.set_value("advance_paid_temp", total_advance);
+      await frm.set_value("outstanding_amount", outstanding_amount);
+    }
+  } catch (err) {
+    console.error("Lỗi khi cập nhật:", err);
+  }
 }
 
 vhtfm.ui.form.on("Sales Taxes and Charges", {
-  rate: function(frm, cdt, cdn) {
-      update_advance_and_outstanding(frm);
+  refresh: function (frm, cdt, cdn) {
+    console.log("Bắt đầu xóa dòng thuế", cdn);
+    // Đánh dấu để xử lý sau khi xóa hoàn tất
+    frm.tax_row_removed = true;
   },
-  tax_amount: function(frm, cdt, cdn) {
-      update_advance_and_outstanding(frm);
+  rate: function (frm, cdt, cdn) {
+    update_advance_and_outstanding(frm);
   },
-  charge_type: function(frm, cdt, cdn) {
-      update_advance_and_outstanding(frm);
+  tax_amount: function (frm, cdt, cdn) {
+    update_advance_and_outstanding(frm);
   },
-  account_head: function(frm, cdt, cdn) {
-      update_advance_and_outstanding(frm);
-  }
+  charge_type: function (frm, cdt, cdn) {
+    update_advance_and_outstanding(frm);
+  },
+  account_head: function (frm, cdt, cdn) {
+    update_advance_and_outstanding(frm);
+  },
+  change: function (frm, cdt, cdn) {
+    console.log("change");
+    update_advance_and_outstanding(frm);
+  },
 });
 
 vhtfm.ui.form.on("Sales Order Item", {
-	qty(frm) {
-		update_advance_and_outstanding(frm);
-	},
-	rate(frm) {
-		update_advance_and_outstanding(frm);
-	}
+  qty(frm) {
+    update_advance_and_outstanding(frm);
+  },
+  rate(frm) {
+    update_advance_and_outstanding(frm);
+  },
 });
 
 vhtfm.ui.form.on("Sales Order", {
-  
   refresh: function (frm) {
     update_advance_and_outstanding(frm);
 
-	// vhtfm.call({
-	// 	method: "xbox_custom.overrides.sales_order.get_advance_paid_actual",
-	// 	args: { so: frm.doc.name },
-	// 	callback(r) {
-	// 		console.log("r.message:", r.message );
-	// 		// frm.set_value("advance_paid_actual", r.message || 0);
-	// 	}
-	// });
+    // vhtfm.call({
+    // 	method: "xbox_custom.overrides.sales_order.get_advance_paid_actual",
+    // 	args: { so: frm.doc.name },
+    // 	callback(r) {
+    // 		console.log("r.message:", r.message );
+    // 		// frm.set_value("advance_paid_actual", r.message || 0);
+    // 	}
+    // });
     frm.fields_dict.items.grid.df.read_only = 0;
     frm.fields_dict.items.grid.df.editable_grid = 1;
     frm.refresh_field("items");
@@ -203,33 +211,31 @@ vhtfm.ui.form.on("Sales Order", {
     }
   },
   taxes_and_charges: function (frm) {
-		update_advance_and_outstanding(frm);
-	},
+    update_advance_and_outstanding(frm);
+  },
 
-	taxes: function (frm) {
-		update_advance_and_outstanding(frm);
-	},
+  taxes: function (frm) {
+    console.log("Tax removed");
+    update_advance_and_outstanding(frm);
+  },
   items_on_form_rendered: function (frm) {
     update_advance_and_outstanding(frm);
   },
-	// Nếu bạn muốn trigger lại khi thay đổi discount hoặc shipping charge, thêm thêm ở đây
-	additional_discount_percentage: function (frm) {
-		update_advance_and_outstanding(frm);
-	},
-  taxes_add: function(frm) {
+  // Nếu bạn muốn trigger lại khi thay đổi discount hoặc shipping charge, thêm thêm ở đây
+  additional_discount_percentage: function (frm) {
     update_advance_and_outstanding(frm);
   },
-  taxes_remove: function(frm) {
-      update_advance_and_outstanding(frm);
+  taxes_add: function (frm) {
+    update_advance_and_outstanding(frm);
   },
-  additional_discount_percentage: function(frm) {
-      update_advance_and_outstanding(frm);
+  additional_discount_percentage: function (frm) {
+    update_advance_and_outstanding(frm);
   },
-  discount_amount: function(frm) {
-      update_advance_and_outstanding(frm);
+  discount_amount: function (frm) {
+    update_advance_and_outstanding(frm);
   },
-  apply_discount_on: function(frm) {
-      update_advance_and_outstanding(frm);
+  apply_discount_on: function (frm) {
+    update_advance_and_outstanding(frm);
   },
 
   onload: function (frm) {
@@ -241,6 +247,24 @@ vhtfm.ui.form.on("Sales Order", {
     frm.custom_onload = function () {
       $(frm.page.wrapper).find(".dropdown-menu a").off("click");
     };
+  },
+  onload_post_render: function (frm) {
+    let grid = frm.fields_dict.taxes.grid;
+    if (!grid.__custom_remove_hooked) {
+      grid.wrapper.on("click", ".grid-delete-row", function () {
+        setTimeout(() => {
+          update_advance_and_outstanding(frm);
+        }, 300);
+      });
+
+      grid.wrapper.on("click", ".grid-remove-rows", function () {
+        setTimeout(() => {
+          update_advance_and_outstanding(frm);
+        }, 300);
+      });
+
+      grid.__custom_remove_hooked = true;
+    }
   },
 });
 
